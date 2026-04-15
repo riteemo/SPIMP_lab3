ec0e7673710d15e139bc7a76385304939796bd88
riteemo <irina.qqu@gmail.com>
2026-04-16 01:38:02 +0300

    add validation & error handling (x unwrap)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use image::Luma;
 use qrcode::QrCode;
 use std::io;
 
@@ -5,10 +6,15 @@ fn main() {
     println!("Введите текст или URL:");
 
     let mut input = String::new();
-    io::stdin().read_line(&mut input).unwrap();
+    io::stdin().read_line(&mut input).expect("Ошибка чтения текста или URL");
     let input = input.trim();
 
-    let code = QrCode::new(input.as_bytes()).unwrap();
+    if input.is_empty() {
+        eprintln!("Введите непустую строку.");
+        return;
+    }
+
+    let code = QrCode::new(input.as_bytes()).expect("Ошибка создания QR-кода");
     let string = code
         .render::<char>()
         .quiet_zone(false)
@@ -16,4 +22,8 @@ fn main() {
         .build();
 
     println!("\nQR-код:\n{}", string);
+
+    let image = code.render::<Luma<u8>>().build();
+    image.save("qr_code.png").expect("Ошибка сохранения qr_code.png");
+    println!("QR-код сохранён в qr_code.png");
 }
