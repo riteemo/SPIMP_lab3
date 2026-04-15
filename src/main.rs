875ed6bc23483d8b2ec0e3b242a6ee931bb097d1
riteemo <irina.qqu@gmail.com>
2026-04-16 01:10:02 +0300

    user input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Input:");
+    let mut input = String::new();
+    io::stdin().read_line(&mut input);
+
+    println!("Output: {}", input.trim());
 }
