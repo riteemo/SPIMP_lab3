90618a659a82abfbde01567e1ea3bc46e3faa9f3
riteemo <irina.qqu@gmail.com>
2026-04-16 01:20:28 +0300

    HOORAY HOORAY HOORAY

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,19 @@
+use qrcode::QrCode;
 use std::io;
 
 fn main() {
-    println!("Input:");
-    let mut input = String::new();
-    io::stdin().read_line(&mut input);
+    println!("Введите текст или URL:");
 
-    println!("Output: {}", input.trim());
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+    let input = input.trim();
+
+    let code = QrCode::new(input.as_bytes()).unwrap();
+    let string = code
+        .render::<char>()
+        .quiet_zone(false)
+        .module_dimensions(2, 1)
+        .build();
+
+    println!("\nQR-код:\n{}", string);
 }
